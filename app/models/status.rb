2e99f342dd0b276f339454ce78eb4db6e0232f51
heguro <heguro@outlook.com>
2020-05-29 05:45:43 +0900

    書き方ダメみたいなので別の書き方で(もっといい書き方あるかもだけど調べるのだるい)

--- app/models/status.rb
+++ app/models/status.rb
@@ -90,7 +90,7 @@ class Status < ApplicationRecord
   scope :without_replies, -> { where('statuses.reply = FALSE OR statuses.in_reply_to_account_id = statuses.account_id') }
   scope :without_reblogs, -> { where('statuses.reblog_of_id IS NULL') }
   scope :with_public_visibility, -> { where(visibility: :public) }
-  scope :with_public_or_unlisted_visibility, -> { where(visibility: { in: %w(:public :unlisted) }) }
+  scope :with_public_or_unlisted_visibility, -> { where(visibility: :public).or(where(visibility: :unlisted)) }
   scope :tagged_with, ->(tag) { joins(:statuses_tags).where(statuses_tags: { tag_id: tag }) }
   scope :excluding_silenced_accounts, -> { left_outer_joins(:account).where(accounts: { silenced_at: nil }) }
   scope :including_silenced_accounts, -> { left_outer_joins(:account).where.not(accounts: { silenced_at: nil }) }
